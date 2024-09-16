2c3921bf33a72056c027ed712c29627b12c3fb90
itsumi <122093200+itsumiq@users.noreply.github.com>
2024-09-16 22:29:47 +0300

    fix: minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: 'https://itsumiq.github.io/',
+  base: './',
 });
