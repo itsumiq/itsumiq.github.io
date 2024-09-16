b86a30f0896dd9385895313a3e05886566f9e8ea
itsumi <122093200+itsumiq@users.noreply.github.com>
2024-09-16 22:18:30 +0300

    fix: minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { AppRouter } from './app/routers/AppRouter';
 
 import { persistor, store } from './app/store/store';
-import './app/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <Home /> */}
         <AppRouter />
       </PersistGate>
     </Provider>
